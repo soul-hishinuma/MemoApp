17dc2a672af168f031c66cf7855e5672bc186cad
soul-hishinuma <soul00@outlook.jp>
2021-01-23 02:28:57 +0900

    first commit

---
+++ notepad/src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from "vue"
+import Vuex from "vuex"
+import createdPersitedState from "vuex-persistedstate"
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  plugins:[createdPersitedState()],
+  state:{
+    memos:[
+      {id:1,body:"サンプルのメモです"}
+    ]
+  },
+  mutations:{
+    save(state,memo){
+      var max = state.memos[state.memos.length - 1].id;
+      memo.id = max + 1;
+      state.memos.push(memo);
+    },
+    update(state,data){
+      let x = state.memos.find(memo => memo.id == data.id);
+      x.body = data.body;
+    }
+  }
+})
